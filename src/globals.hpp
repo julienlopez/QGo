a97a7c97600fd07286997e896990a879e10f7301
julien lopez <julien.lopez51@gmail.com>
2013-11-21 21:33:04 +0100

    cleaning up some code

--- src/globals.hpp
+++ src/globals.hpp
@@ -1,19 +1,13 @@
 #ifndef GLOBALS_HPP
 #define GLOBALS_HPP
 
-#include <inttypes.h>
+#include <cinttypes>
 
 #include <memory>
 #include <list>
+#include <set>
 
-typedef signed char 	int8_t;
-//typedef unsigned char 	uint8_t;
-//typedef signed int 	int16_t;
-//typedef unsigned int 	uint16_t;
-//typedef signed long int 	int32_t;
-//typedef unsigned long int 	uint32_t;
-//typedef signed long long int 	int64_t;
-//typedef unsigned long long int 	uint64_t;
+typedef signed char int8_t;
 
 class Goban;
 class Point;
@@ -23,7 +17,9 @@ namespace QGo
     typedef std::shared_ptr<Goban> goban_sp;
     typedef std::weak_ptr<Goban> goban_wp;
 
-    typedef std::list<Point> type_list_point;
+    using type_list_point = std::list<Point>;
+
+    using type_set_point = std::set<Point>;
 
     enum Case {EMPTY = 0, BLACK, WHITE};
 }
