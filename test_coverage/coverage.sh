ac39ded573fc0bd8dc34b1855060b438ad40d7d8
julien lopez <julien.lopez51@gmail.com>
2013-11-21 21:33:47 +0100

    making code coverage work properly

--- test_coverage/coverage.sh
+++ test_coverage/coverage.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+dir=`pwd`
+root_dir=$(readlink -m ..)
+
 rm *.gcov
 #rm ../obj_tests/*
 
 #find out how to compile the tests
 
-lcov --zerocounters --directory ../obj_tests --directory .
-lcov --capture --initial --directory ../obj_tests --directory . --output-file coverage.info --no-external
+lcov --zerocounters --directory $root_dir/obj_tests --directory $dir --no-external
+lcov --capture --initial --directory $root_dir/obj_tests --directory $dir --output-file coverage.info --no-external
 
 echo "running tests"
-../bin/tests
+../bin/tests || { exit 1; }
 echo "tests done"
 
 echo "running gcov"
-gcov ../src/*.cpp -o ../obj_tests
+gcov ../src/*.cpp -o $root_dir/obj_tests
 echo "gcov done"
 
 echo "running lcov"
-lcov --capture --directory ../obj_tests --directory . --output-file coverage.info --no-checksum
+lcov --capture --directory $root_dir/obj_tests --directory $dir --directory $root_dir/src --output-file coverage.info --no-checksum --no-external
 genhtml -o html coverage.info
 echo "lcov done"
-
